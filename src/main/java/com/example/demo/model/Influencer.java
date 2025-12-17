97bdd7ea4d19f2d441f51ede9064c914c06fc04d
bala-amypo <bala@amypo.in>
2025-12-17 08:27:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Influencer.java
@@ -0,0 +1,10 @@
+package com.example.demo.dto;
+
+public class Influencer{
+    private long id;
+    private String name;
+    private String socialHandle;
+    private String email;
+    private Boolean active;
+    
+}
