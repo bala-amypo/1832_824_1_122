2b4b27062dd01abe9c1a5925f759fffd5e8a01ac
bala-amypo <bala@amypo.in>
2025-12-17 09:06:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/DiscountCode.java
@@ -0,0 +1,57 @@
+public class DiscountCode {
+
+    private Long id;
+    private String code;
+    private Influencer influencer;
+    private Campaign campaign;
+    private Double discountPercentage;
+    private Boolean active;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Influencer getInfluencer() {
+        return influencer;
+    }
+
+    public void setInfluencer(Influencer influencer) {
+        this.influencer = influencer;
+    }
+
+    public Campaign getCampaign() {
+        return campaign;
+    }
+
+    public void setCampaign(Campaign campaign) {
+        this.campaign = campaign;
+    }
+
+    public Double getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(Double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
