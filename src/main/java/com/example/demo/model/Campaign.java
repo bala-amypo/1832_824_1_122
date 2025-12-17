6a9ae4f64f9f4ccb38ef45ed4edf47c4ad924f1e
bala-amypo <bala@amypo.in>
2025-12-17 08:53:43 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Campaign.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto;
+
+public class Campaign{
+    private Long id;
+    private String CampaignName;
+    private Date startDate;
+    private BigDecimal budget;
+    private Boolean active;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long )
+}
