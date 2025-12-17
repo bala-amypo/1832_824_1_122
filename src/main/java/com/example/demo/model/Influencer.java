2c3bc6ffa45ffc4a243aefa0f9e1e1475faa86f3
bala-amypo <bala@amypo.in>
2025-12-17 08:32:57 +0000

    User commit

--- src/main/java/com/example/demo/model/Influencer.java
+++ src/main/java/com/example/demo/model/Influencer.java
@@ -1,10 +1,28 @@
 package com.example.demo.dto;
 
 public class Influencer{
-    private long id;
+    private Long id;
     private String name;
     private String socialHandle;
     private String email;
     private Boolean active;
-    
+    private Timestamp createdAt;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(long id){
+        this.id=id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String 
 }
