35220d60a811ab9c1aac68e53643ca0a75e875d3
bala-amypo <bala@amypo.in>
2025-12-17 08:57:58 +0000

    User commit

--- src/main/java/com/example/demo/model/Campaign.java
+++ src/main/java/com/example/demo/model/Campaign.java
@@ -2,7 +2,7 @@ package com.example.demo.dto;
 
 public class Campaign{
     private Long id;
-    private String CampaignName;
+    private String campaignName;
     private Date startDate;
     private BigDecimal budget;
     private Boolean active;
@@ -11,5 +11,23 @@ public class Campaign{
         return id;
     }
 
-    public void setId(Long )
+    public void setId(Long id){
+        this.id=id;
+    }
+
+    public String getCampaignName(){
+        return campaignName;
+    }
+
+    public void setcampaignName(String campaignName){
+        this.campaignName=campaignName;
+    }
+
+    public Date getStartDate(){
+        return startDate;
+    }
+
+    public void getStartDate(Date startDate){
+        this.
+    }
 }
