aea9a66fd246cfda9271b8497db8f17475d4a87b
bala-amypo <bala@amypo.in>
2025-12-18 09:08:04 +0000

    User commit

--- src/main/java/com/example/demo/model/DiscountCode.java
+++ src/main/java/com/example/demo/model/DiscountCode.java
@@ -1,71 +1,71 @@
-package com.example.demo.dto;
-import jakarta.presistence.Entity;
-import jakarta.presistence.Id;
-@Entity
-public class DiscountCode {
-    @Id
-    private Long id;
-    private String code;
-    private String influencer;
-    private String campaign;
-    private Double discountPercentage;
-    private Boolean active;
+// package com.example.demo.dto;
+// import jakarta.presistence.Entity;
+// import jakarta.presistence.Id;
+// @Entity
+// public class DiscountCode {
+//     @Id
+//     private Long id;
+//     private String code;
+//     private String influencer;
+//     private String campaign;
+//     private Double discountPercentage;
+//     private Boolean active;
 
-    public Long getId() {
-        return id;
-    }
+//     public Long getId() {
+//         return id;
+//     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
 
-    public String getCode() {
-        return code;
-    }
+//     public String getCode() {
+//         return code;
+//     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
+//     public void setCode(String code) {
+//         this.code = code;
+//     }
 
-    public String getInfluencer() {
-        return influencer;
-    }
+//     public String getInfluencer() {
+//         return influencer;
+//     }
 
-    public void setInfluencer(String influencer) {
-        this.influencer = influencer;
-    }
+//     public void setInfluencer(String influencer) {
+//         this.influencer = influencer;
+//     }
 
-    public String getCampaign() {
-        return campaign;
-    }
+//     public String getCampaign() {
+//         return campaign;
+//     }
 
-    public void setCampaign(String campaign) {
-        this.campaign = campaign;
-    }
+//     public void setCampaign(String campaign) {
+//         this.campaign = campaign;
+//     }
 
-    public Double getDiscountPercentage() {
-        return discountPercentage;
-    }
+//     public Double getDiscountPercentage() {
+//         return discountPercentage;
+//     }
 
-    public void setDiscountPercentage(Double discountPercentage) {
-        this.discountPercentage = discountPercentage;
-    }
+//     public void setDiscountPercentage(Double discountPercentage) {
+//         this.discountPercentage = discountPercentage;
+//     }
 
-    public Boolean getActive() {
-        return active;
-    }
+//     public Boolean getActive() {
+//         return active;
+//     }
 
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-public DiscountCode(Long id,String code,String Influencer,String campaign,Double discountPercentage,Boolean active)
-this.id=id;
-this.code-code;
-this.Influencer=Influencer;
-this.campaign=campaign;
-this.discountPercentage=discountPercentage;
-this.active=active;
-}
-public DiscountCode(){
+//     public void setActive(Boolean active) {
+//         this.active = active;
+//     }
+// public DiscountCode(Long id,String code,String Influencer,String campaign,Double discountPercentage,Boolean active)
+// this.id=id;
+// this.code-code;
+// this.Influencer=Influencer;
+// this.campaign=campaign;
+// this.discountPercentage=discountPercentage;
+// this.active=active;
+// }
+// public DiscountCode(){
 
-}
+// }
