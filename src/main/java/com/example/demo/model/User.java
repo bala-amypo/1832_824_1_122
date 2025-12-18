fc433aa1f3c6dd36a2d7ba953560b6d7764fb635
bala-amypo <bala@amypo.in>
2025-12-18 08:53:03 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,72 +1,72 @@
-// package com.example.demo.dto;
-// import jakarta.presistence.Entity;
-// import jakarta.presistence.Id;
-// import java.time.LocalDateTime;
-// @Entity
-// public class User {
-//     @Id
-//     private Long id;
-//     private String fullName;
-//     private String email;
-//     private String password;
-//     private String role;
-//     private LocalDateTime createdAt;
+package com.example.demo.dto;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+@Entity
+public class User {
+    @Id
+        private Long id;
+            private String fullName;
+                private String email;
+                    private String password;
+                        private String role;
+                            private LocalDateTime createdAt;
 
-//     public Long getId() {
-//         return id;
-//     }
+                                public Long getId() {
+                                        return id;
+                                            }
 
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
+                                                public void setId(Long id) {
+                                                        this.id = id;
+                                                            }
 
-//     public String getFullName() {
-//         return fullName;
-//     }
+                                                                public String getFullName() {
+                                                                        return fullName;
+                                                                            }
 
-//     public void setFullName(String fullName) {
-//         this.fullName = fullName;
-//     }
+                                                                                public void setFullName(String fullName) {
+                                                                                        this.fullName = fullName;
+                                                                                            }
 
-//     public String getEmail() {
-//         return email;
-//     }
+                                                                                                public String getEmail() {
+                                                                                                        return email;
+                                                                                                            }
 
-//     public void setEmail(String email) {
-//         this.email = email;
-//     }
+                                                                                                                public void setEmail(String email) {
+                                                                                                                        this.email = email;
+                                                                                                                            }
 
-//     public String getPassword() {
-//         return password;
-//     }
+                                                                                                                                public String getPassword() {
+                                                                                                                                        return password;
+                                                                                                                                            }
 
-//     public void setPassword(String password) {
-//         this.password = password;
-//     }
+                                                                                                                                                public void setPassword(String password) {
+                                                                                                                                                        this.password = password;
+                                                                                                                                                            }
 
-//     public String getRole() {
-//         return role;
-//     }
+                                                                                                                                                                public String getRole() {
+                                                                                                                                                                        return role;
+                                                                                                                                                                            }
 
-//     public void setRole(String role) {
-//         this.role = role;
-//     }
+                                                                                                                                                                                public void setRole(String role) {
+                                                                                                                                                                                        this.role = role;
+                                                                                                                                                                                            }
 
-//     public LocalDateTime getCreatedAt() {
-//         return createdAt;
-//     }
+                                                                                                                                                                                                public LocalDateTime getCreatedAt() {
+                                                                                                                                                                                                        return createdAt;
+                                                                                                                                                                                                            }
 
-//     public void setCreatedAt(LocalDateTime createdAt) {
-//         this.createdAt = createdAt;
-//     }
-//     public User(Long id,String fullName,String email,String password,String role,LocalDateTime createdAt)
-//     this.id=id;
-//     this.fullName=fullName;
-//     this.email=email;
-//     this.password=password;
-//     this.role=role;
-//     this.createdAt=createdAt;
-// }
-// public User(){
+                                                                                                                                                                                                                public void setCreatedAt(LocalDateTime createdAt) {
+                                                                                                                                                                                                                        this.createdAt = createdAt;
+                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                public User(Long id,String fullName,String email,String password,String role,LocalDateTime createdAt)
+                                                                                                                                                                                                                                    this.id= id;
+                                                                                                                                                                                                                                        this.fullName= fullName;
+                                                                                                                                                                                                                                            this.email= email;
+                                                                                                                                                                                                                                                this.password= password;
+                                                                                                                                                                                                                                                    this.role= role;
+                                                                                                                                                                                                                                                        this.createdAt= createdAt;
+                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                        public User(){
 
-// }
+                                                                                                                                                                                                                                                        }
