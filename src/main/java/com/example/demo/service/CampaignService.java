207379f71eb1c55fdb721df3becc0f3caa64cddd
bala-amypo <bala@amypo.in>
2025-12-19 08:13:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/CampaignService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+import com.example.demo.entity.TaskAssignment;
+
+public interface   TaskAssignmentService
+{
+    TaskAssignmentServices createTaskAssignment(TaskAssignment ts);
+
+
+}
---
+++ src/main/java/com/example/demo/service/InfluencerService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+import com.example.demo.entity.Influencer;
+
+public interface   InfluencerService
+{
+    InfluencerServices createTaskAssignment(Influencer in);
+
+
+}
