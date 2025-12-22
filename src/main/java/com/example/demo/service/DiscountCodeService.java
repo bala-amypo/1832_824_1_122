27fda7c50dbe3097e3ee33ae052338e82b38e12c
bala-amypo <bala@amypo.in>
2025-12-22 07:43:49 +0000

    User commit

--- src/main/java/com/example/demo/service/DiscountCodeService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-import com.example.demo.entity.DiscountCode;
-
-public interface   DiscountCodeService
-{
-    DiscountCodeServices createDiscountCode(DiscountCode dc);
-
-
-}
--- src/main/java/com/example/demo/service/SaleTransactionService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-import com.example.demo.entity.SaleTransaction;
-
-public interface   SaleTransactionService
-{
-    SaleTransactionServices createSaleTransaction(SaleTransaction st);
-
-
-}
