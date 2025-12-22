507c5fa6e21d5d64fc14c66803a9ede326b612e5
bala-amypo <bala@amypo.in>
2025-12-22 20:12:09 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,12 +1,12 @@
-@EnableWebSecurity
-public class SecurityConfig {
-@Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-http.csrf().disable()
-.authorizeHttpRequests()
-.requestMatchers("/auth/**", "/swagger-ui/**").permitAll()
-.anyRequest().authenticated()
-.and().httpBasic();
-return http.build();
-}
-}
+// @EnableWebSecurity
+// public class SecurityConfig {
+// @Bean
+// public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+// http.csrf().disable()
+// .authorizeHttpRequests()
+// .requestMatchers("/auth/**", "/swagger-ui/**").permitAll()
+// .anyRequest().authenticated()
+// .and().httpBasic();
+// return http.build();
+// }
+// }
--- src/main/java/com/example/demo/repository/RoiReportRepository.java
+++ src/main/java/com/example/demo/repository/RoiReportRepository.java
@@ -1,1 +1,7 @@
-public interface RoiReportRepository extends JpaRepository<RoiReport, Long> {}
+package com.example.demo.repository;
+
+import com.example.demo.entity.RoiReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoiReportRepository extends JpaRepository<RoiReport, Long> {
+}
