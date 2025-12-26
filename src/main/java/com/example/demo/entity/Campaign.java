ca47677bd1400ad0b92b9a9d52c489e45ee96e9b
bala-amypo <bala@amypo.in>
2025-12-26 08:33:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/Campaign.java
+++ src/main/java/com/example/demo/entity/Campaign.java
@@ -1,62 +1,9 @@
-package com.example.demo.model;
+private Double budget;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import java.time.LocalDate;
+public Double getBudget() {
+    return budget;
+}
 
-@Entity
-@Table(name = "campaigns")
-public class Campaign {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String campaignName;
-    private LocalDate startDate;
-    private LocalDate endDate;
-
-    public Campaign() {
-    }
-
-    public Campaign(String campaignName, LocalDate startDate, LocalDate endDate) {
-        this.campaignName = campaignName;
-        this.startDate = startDate;
-        this.endDate = endDate;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getCampaignName() {
-        return campaignName;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setCampaignName(String campaignName) {
-        this.campaignName = campaignName;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
+public void setBudget(Double budget) {
+    this.budget = budget;
 }
