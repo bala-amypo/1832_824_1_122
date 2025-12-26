deeb34228a5e25656ef1d54524612d2320a4130d
bala-amypo <bala@amypo.in>
2025-12-26 08:27:09 +0000

    User commit

--- src/main/java/com/example/demo/service/CampaignService.java
+++ src/main/java/com/example/demo/service/CampaignService.java
@@ -1,10 +1,47 @@
 package com.example.demo.service;
 
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Campaign;
+import com.example.demo.repository.CampaignRepository;
 import java.util.List;
+import org.springframework.stereotype.Service;
 
-public interface CampaignService {
-    Campaign updateCampaign(Long id, Campaign campaign);
-    Campaign getCampaignById(Long id);
-    List<Campaign> getAllCampaigns();
+@Service
+public class CampaignService {
+
+    private final CampaignRepository campaignRepository;
+
+    public CampaignService(CampaignRepository campaignRepository) {
+        this.campaignRepository = campaignRepository;
+    }
+
+    public Campaign createCampaign(Campaign campaign) {
+        return campaignRepository.save(campaign);
+    }
+
+    public List<Campaign> getAllCampaigns() {
+        return campaignRepository.findAll();
+    }
+
+    public Campaign getCampaignById(Long id) {
+        return campaignRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Campaign not found"));
+    }
+
+    public Campaign updateCampaign(Long id, Campaign updatedCampaign) {
+        Campaign existing = getCampaignById(id);
+
+        existing.setCampaignName(updatedCampaign.getCampaignName());
+        existing.setBudget(updatedCampaign.getBudget());
+        existing.setStartDate(updatedCampaign.getStartDate());
+        existing.setEndDate(updatedCampaign.getEndDate());
+
+        return campaignRepository.save(existing);
+    }
+
+    public void deleteCampaign(Long id) {
+        Campaign campaign = getCampaignById(id);
+        campaignRepository.delete(campaign);
+    }
 }
